0bf50e02a8c140afdde3dac5b4df8d45ee514284
MHD <m@codiume.com>
2026-03-15 01:14:18 +0100

    Add php8.5 (#13)

--- src/config.ts
+++ src/config.ts
@@ -10,29 +10,34 @@ export const mainInstall = `curl -fsSL ${url}/install.sh | bash`;
 
 export const versions = [
   {
-    id: "php74",
+    id: "php7.4",
     preset: "PHP7.4",
     code: `curl -fsSL ${url}/7.4/install.sh | bash`,
   },
   {
-    id: "php81",
+    id: "php8.1",
     preset: "PHP8.1",
     code: `curl -fsSL ${url}/8.1/install.sh | bash`,
   },
   {
-    id: "php82",
+    id: "php8.2",
     preset: "PHP8.2",
     code: `curl -fsSL ${url}/8.2/install.sh | bash`,
   },
   {
-    id: "php83",
+    id: "php8.3",
     preset: "PHP8.3",
     code: `curl -fsSL ${url}/8.3/install.sh | bash`,
   },
   {
-    id: "php84",
+    id: "php8.4",
     preset: "PHP8.4",
     code: `curl -fsSL ${url}/8.4/install.sh | bash`,
+  },
+  {
+    id: "php8.5",
+    preset: "PHP8.5",
+    code: `curl -fsSL ${url}/8.5/install.sh | bash`,
   },
 ];
 
--- src/pages/[version]/install.sh.ts
+++ src/pages/[version]/install.sh.ts
@@ -2,14 +2,16 @@ import type { APIRoute } from "astro";
 
 import { versions } from "../../config";
 
-import stub from "../../presets/php.sh?raw";
+import stub from "../../scripts/php.sh?raw";
 
 export function getStaticPaths() {
-  return versions.map((v) => ({ params: { version: v.id } }));
+  return versions.map((v) => ({
+    params: { version: v.id.replace("php", "") },
+  }));
 }
 
 export const GET: APIRoute = async ({ params }) => {
-  const content = stub.replace("8.4", params.version as string);
+  const content = stub.replace("8.5", params.version as string);
   return new Response(content, {
     headers: {
       "Content-Type": "text/plain;charset=UTF-8",
